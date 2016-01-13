f83b67af9a96fe9a6124e3ea5319fc258ff8ab56
Luke <lflint07@gmail.com>
2016-01-13 16:33:23 -0500

    added structure to the layouts

--- app/controllers/donuts_controller.rb
+++ app/controllers/donuts_controller.rb
@@ -68,7 +68,7 @@ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_donut
-      @donut = Donuts.find(params[:id])
+      @donut = Donut.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
